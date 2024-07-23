e32bd3920508fe0638127a154c1cac63a2f331f6
dhailong267 <dhailong267@gmail.com>
2024-07-23 01:09:31 +0000

    Initial commit

--- contracts/Hello_swtr.sol
+++ contracts/Hello_swtr.sol
@@ -1,34 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.19;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+//This contract is only intended for testing purposes
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract Swisstronik {
+    string private message;
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    /**
+     * @dev Constructor is used to set the initial message for the contract
+     * @param _message the message to associate with the message variable.
+     */
+    constructor(string memory _message) payable{
+        message = _message;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    /**
+     * @dev setMessage() updates the stored message in the contract
+     * @param _message the new message to replace the existing one
+     */
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    /**
+     * @dev getMessage() retrieves the currently stored message in the contract
+     * @return The message associated with the contract
+     */
+    function getMessage() public view returns(string memory){
+        return message;
     }
 }
