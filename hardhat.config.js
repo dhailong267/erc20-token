ea7d05617236e7c64719e57c0eb01c025076e014
dhailong267 <83217500+dhailong267@users.noreply.github.com>
2024-07-24 18:21:37 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", // URL of the RPC node for Swisstronik.
-      accounts: ["0xbee5794df85249da1527a37245d344169f0a3ed04de009ba4900230cb2234769"] // Your private key starting with "0x"
+      accounts: ["0x"] // Your private key starting with "0x"
       // Make sure you have enough funds in this wallet to deploy the smart contract
     }
   }
